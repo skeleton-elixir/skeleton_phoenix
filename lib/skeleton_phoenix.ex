79924362962daab16283d2565bc0b3595b201294
Diego Nogueira <diegoalvareznogueira@gmail.com>
2020-06-27 20:18:40 -0300

    Initial commit

---
+++ lib/skeleton_phoenix.ex
@@ -0,0 +1,18 @@
+defmodule SkeletonPhoenix do
+  @moduledoc """
+  Documentation for `SkeletonPhoenix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SkeletonPhoenix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/skeleton_phoenix_test.exs
@@ -0,0 +1,8 @@
+defmodule SkeletonPhoenixTest do
+  use ExUnit.Case
+  doctest SkeletonPhoenix
+
+  test "greets the world" do
+    assert SkeletonPhoenix.hello() == :world
+  end
+end
