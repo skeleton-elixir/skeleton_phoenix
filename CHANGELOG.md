6b2e38226290c1e2857a8399044f725673ea3707
Jhonathas Matos <jhonathas@gmail.com>
2021-06-30 21:29:58 -0300

    Release v2.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+2.0.1
+------
+#### Changes
+* Changes the behavior of the "ensure_not_authenticated" function
+
 2.0.0
 ------
 #### Changes
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SkeletonPhoenix.MixProject do
   use Mix.Project
 
-  @version "2.0.0"
+  @version "2.0.1"
   @source_url "https://github.com/skeleton-elixir/skeleton_phoenix"
   @maintainers [
     "Diego Nogueira",
